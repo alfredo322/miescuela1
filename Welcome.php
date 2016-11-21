0883d461fc31d18f2feb9441ef6b9e0b004a5343
alfredo322 <nobrain173@gmail.com>
2016-11-21 18:37:34 -0500

    Update Welcome.php

--- Welcome.php
+++ Welcome.php
@@ -26,7 +26,7 @@ class Welcome extends CI_Controller {
 	{
 		$query = ''; //declaramos para evitar advertencias de PHP
 		$nombre = $this->input->post('Nombres', TRUE);
-		$sql= "insert into maestra(nombres) values($nombre)  ";
+		$sql= "insert into maestra(nombres) values('$nombre')  ";
 		$query = $this->db->query($sql, array($query));
 		redirect('/controlador/formulario', 'refresh'); //redirecciona para seguir insertando
  	}
@@ -34,7 +34,7 @@ class Welcome extends CI_Controller {
 	{
 		$query = ''; //declaramos para evitar advertencias de PHP
 		$apellidos = $this->input->post('Apellidos', TRUE);
-		$sql= "insert into maestra(apellidos) values($apellidos)  ";
+		$sql= "insert into maestra(apellidos) values('$apellidos')  ";
 		$query = $this->db->query($sql, array($query));
 		redirect('/controlador/formulario', 'refresh'); //redirecciona para seguir insertando
  	}
@@ -42,7 +42,7 @@ class Welcome extends CI_Controller {
 	{
 		$query = ''; //declaramos para evitar advertencias de PHP
 		$edad = $this->input->post('Edad', TRUE);
-		$sql= "insert into maestra(edad) values($edad)  ";
+		$sql= "insert into maestra(edad) values('$edad')  ";
 		$query = $this->db->query($sql, array($query));
 		redirect('/controlador/formulario', 'refresh'); //redirecciona para seguir insertando
  	}
@@ -50,7 +50,7 @@ class Welcome extends CI_Controller {
 	{
 		$query = ''; //declaramos para evitar advertencias de PHP
 		$fecha = $this->input->post('Fecha_Nacimiento', TRUE);
-		$sql= "insert into maestra(fecha_nacimiento) values($fecha)  ";
+		$sql= "insert into maestra(fecha_nacimiento) values('$fecha')  ";
 		$query = $this->db->query($sql, array($query));
 		redirect('/controlador/formulario', 'refresh'); //redirecciona para seguir insertando
  	}
@@ -58,7 +58,7 @@ class Welcome extends CI_Controller {
 	{
 		$query = ''; //declaramos para evitar advertencias de PHP
 		$celular = $this->input->post('Celular', TRUE);
-		$sql= "insert into detalle(celular) values($celular)  ";
+		$sql= "insert into detalle(celular) values('$celular')  ";
 		$query = $this->db->query($sql, array($query));
 		redirect('/controlador/formulario', 'refresh'); //redirecciona para seguir insertando
  	}
